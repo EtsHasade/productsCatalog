a4182c64e8d7e682d7e0f7fd629140034656ce65
Nadav <nadavkomo@gmail.com>
2020-12-10 16:58:50 +0200

    filter

--- src/cmp/product-filter.vue
+++ src/cmp/product-filter.vue
@@ -1,11 +1,35 @@
 <template>
-    <section class="product-filter">
-
-    </section>
+  <section class="product-filter">
+      <form @submit.prevent="emitFilter">
+          <select v-model="filterBy.byGender">
+              <option value="him">Him</option>
+              <option value="her">Her</option>
+              <option value="both">Both</option>
+          </select>
+          <select price>
+              <option value=""></option>
+          </select>
+      </form>
+  </section>
 </template>
 
 <script>
 export default {
-    name: 'product-filter'
+  name: 'product-filter',
+  data() {
+    return {
+      price: null,
+      filterBy: { byGender: 'both', price: { from: 0, to: Infinity } }
+    }
+  },
+  methods: {
+    emitFilter() {
+      if (this.debounce) clearTimeout(this.debounce);
+      this.debounce = setTimeout(() => {
+        this.$emit("doFilter", JSON.parse(JSON.stringify(this.filterBy)));
+      }, 1000)
+    }
+  },
+
 }
 </script>
